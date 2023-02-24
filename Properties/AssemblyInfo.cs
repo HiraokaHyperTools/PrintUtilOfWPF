ea7671e1c365d8c3937530ac1776bcc8fbe16682
kenjiuno <ku@digitaldolphins.jp>
2023-02-24 20:44:11 +0900

    0.6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
+
+[assembly: AssemblyFileVersion("0.6")]
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
@@ -10,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HIRAOKA HYPER TOOLS, Inc.")]
 [assembly: AssemblyProduct("PrintUtilOfWPF")]
-[assembly: AssemblyCopyright("Copyright (c) 2017")]
+[assembly: AssemblyCopyright("Copyright (c) 2017, 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +51,4 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly:     AssemblyVersion("0.3")]//here
-[assembly: AssemblyFileVersion("0.3")]//here
+[assembly: AssemblyVersion("1.0.0.0")]
